3a495735f629933c0d03e940d2da765b787d7e3d
DimdimichCh <db0279590@gmail.com>
2025-10-23 17:26:13 +0200

    Added error handler if user not found

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS users (
     );
 
 CREATE TABLE IF NOT EXISTS teams (
-    name TEXT
+    name TEXT UNIQUE
     );
 """)
 db.commit()
@@ -27,9 +27,24 @@ while True:
         print("There is all commands that you can write:")
         print("add <username> <team> <bet> - adding new user or new bet of user")
         print("delete <username> <team> - delete user's bet")
-        print("user - find a user and all the bets they have made")
-        print("add team - add new team")
+        print(" users - find all users and all the bets they have made")
+        print(" user <username> - find a user and all the bets they have made")
+        print("add team <team name> - add new team")
         print("teams - find all teams that exists")
+    elif command.startswith("add team"):
+        command = command.split(" ", 3)
+        if len(command) == 3:
+            team_name = command[2]
+            try:
+                c.execute("INSERT INTO teams (name) VALUES (?)", (team_name, ))
+                db.commit()
+            except sqlite3.IntegrityError:
+                print(f"ERROR: Team {team_name} is already exists")
+                continue
+            print(f"Team {team_name} is added")
+        else:
+            print("add team <team name>")
+
     elif command.startswith("add"):
         command = command.split(" ", 4)
         if len(command) == 4 and command[1] != 'team':
@@ -45,23 +60,39 @@ while True:
             if not answer:
                 print("team is not exists, write help for help")
             else:
-                c.execute("""INSERT INTO users (username, choosen_team, bet)
-                            VALUES (?, ?, ?)
-                            ON CONFLICT(username, choosen_team)
-                            DO UPDATE SET bet = excluded.bet;""", (name, team, bet))
+                try:
+                    c.execute("""INSERT INTO users (username, choosen_team, bet) VALUES (?, ?, ?)""", 
+                            (name, team, bet)
+                    )
+                except sqlite3.IntegrityError:
+                    print("User exists already")
+                    
+                db.commit()
+                print(f"User {name}'s bet on the {team}'s team is registered. Bet: {bet} XP")
         else:
             print("add <username> <team> <bet>")
+
+    elif command == "teams":
+        c.execute("SELECT * FROM teams")
+        answer = c.fetchall()
+        print("All Teams:")
+        for a in answer:
+            print(a[0])
 
     elif command.startswith("delete"):
         command = command.split(" ", 3)
         if len(command) == 3:
             username = command[1]
             team = command[2]
-            c.execute("DELETE * FROM users WHERE username = ? AND choosen_team = ?", (username, team))
+            try:
+                c.execute("DELETE * FROM users WHERE username = ? AND choosen_team = ?", (username, team))
+            except Exception as e:
+                print(e)
+                continue
             if c.rowcount > 0:
                 print(f"Bet {username} on {team} is deleted.")
             else:
-                print(f"Bet {username} on {team} is not exists ")
+                print(f"Bet {username} on {team} is not exists")
 
         else:
             print("delete <username> <team>")
